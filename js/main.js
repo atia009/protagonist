51c59b04096dfd596bcce98afac4d27110b74c49
atia009 <aarontia009@gmail.com>
2022-02-21 20:18:57 -0800

    Created list of items for collection and stored in collectionList variable.

---
+++ js/main.js
@@ -0,0 +1,122 @@
+// variables
+const collectionList = [
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Niels Standard SS`,
+        price: 75,
+        src: `../images/collection-01.webp`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Johannes Pocket LS`,
+        price: 150,
+        src: `../images/collection-02.jpg`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Osvald Oxford`,
+        price: 175,
+        src: `../images/collection-03.webp`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Vagn Classic Crew`,
+        price: 175,
+        src: `../images/collection-04.jpg`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Sigfred Lambswool`,
+        price: 185,
+        src: `../images/collection-05.jpg`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Norse Slim Denim`,
+        price: 220,
+        src: `../images/collection-06.jpg`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Aros Slim Light Stretch`,
+        price: 185,
+        src: `../images/collection-07.jpg`,
+    },
+    {
+        brand: `Norse Projects`,
+        category: `clothing`,
+        name: `Aros Slim Light Stretch`,
+        price: 185,
+        src: `../images/collection-08.webp`,
+    },
+    {
+        brand: `A.P.C.`,
+        category: `clothing`,
+        name: `Stephano Cotton-Corduroy Overshirt`,
+        price: 220,
+        src: `../images/collection-09.webp`,
+    },
+    {
+        brand: `A.P.C.`,
+        category: `clothing`,
+        name: `Gaspard Cotton-Twill Bomber Jacket`,
+        price: 220,
+        src: `../images/collection-10.webp`,
+    },
+    {
+        brand: `Common Projects`,
+        category: `shoes`,
+        name: `Original Achilles Leather Sneakers`,
+        price: 440,
+        src: `../images/collection-11.webp`,
+
+    },
+    {
+        brand: `Common Projects`,
+        category: `shoes`,
+        name: `Original Achilles Leather Sneakers`,
+        price: 440,
+        src: `../images/collection-12.webp`,
+
+    },
+    {
+        brand: `R.M. Williams`,
+        category: `shoes`,
+        name: `Craftsman Leather Chelsea Boots`,
+        price: 495,
+        src: `../images/collection-13.webp`,
+
+    },
+    {
+        brand: `Grand Seiko`,
+        category: `accessories`,
+        name: `SBGV009`,
+        price: 3550,
+        src: `../images/collection-14.webp`,
+    },
+    {
+        brand: `A.P.C.`,
+        category: `accessories`,
+        name: `Silver-Tone Chain Bracelet`,
+        price: 155,
+        src: `../images/collection-15.webp`,
+    },
+    {
+        brand: `Miansai`,
+        category: `accessories`,
+        name: `Thin Geo Sterling Silver Ring`,
+        price: 125,
+        src: `../images/collection-16.webp`,
+    },
+]
+
+// functions
+
+// event listeners
